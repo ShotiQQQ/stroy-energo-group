2df8320091549479540b2d3732e923abea260ce4
Denis <shotiqqq@gmail.com>
2023-01-14 17:30:19 +0700

    add ready modal

--- src/js/main.js
+++ src/js/main.js
@@ -47,12 +47,40 @@ links.forEach((link) => {
 const cards = document.querySelectorAll('.directions__card');
 const close = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container');
+const form = document.querySelector('#form');
+
+function clearPopupContainer() {
+  while (popupContainer.firstChild) {
+    popupContainer.removeChild(popupContainer.firstChild);
+  }
+}
+
+function makePopup(e) {
+  const img = e.currentTarget.querySelector('.directions__img').cloneNode(true);
+  const title = e.currentTarget.querySelector('.directions__subtitle').cloneNode(true);
+  const descr = e.currentTarget.querySelector('.directions__descr-full' ).cloneNode(true);
+
+  img.src = img.src.slice(0, -4) + '-720.jpg';
+
+  img.classList.add('popup__img');
+  title.classList.add('popup__title');
+  descr.classList.add('popup__descr');
+
+  popupContainer.append(img, title, descr, form.cloneNode(true));
+  popup.classList.add('popup--active');
+  document.body.style.zIndex = '-10';
+}
+
 cards.forEach((card) => {
-  card.addEventListener('click', () => {
-    popup.classList.add('popup--active');
+  card.addEventListener('click', (e) => {
+    clearPopupContainer();
+    makePopup(e)
   })
 });
 
 close.addEventListener('click', () => {
   popup.classList.remove('popup--active');
+  clearPopupContainer();
 })
+
